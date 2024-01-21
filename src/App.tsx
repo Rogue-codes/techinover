ec3a105ad68aef9ab23f02e0201b66568c59b96a
Rogue-codes <nnamdidanielosuji@gmail.com>
2024-01-21 07:00:46 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import CareersInYourHand from "./components/carreersInYourHand/CareersInYourHand
 import Footer from "./components/footer/Footer";
 import Hero from "./components/hero/Hero";
 import Join from "./components/join/Join";
-import Nav from "./components/nav/Nav";
+// import Nav from "./components/nav/Nav";
 import Stats from "./components/stats/Stats";
 import WeHelp from "./components/wehelp/WeHelp";
 
